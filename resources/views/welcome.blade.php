d518990e61c8b295864b4dc0af9976c85670126d
Hieu Nguyen <thohieu@vnpt.vn>
2025-12-17 16:18:07 +0700

    first commit staging

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel made by Hieu
                 </div>
 
                 <div class="links">
